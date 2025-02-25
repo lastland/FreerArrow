8cce6be77e9af9db709fa461d5a1f995e8a4e393
Yao Li <hnkfliyao@gmail.com>
2025-02-24 19:26:32 -0800

    Arrow hierarchy re-presented.

---
+++ src/Control/Arrow/Arrow.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
+module Control.Arrow.Arrow where
+
+import Prelude hiding (id, (.))
+import Data.Kind
+
+{-- begin Arrows --}
+class Profunctor (p :: Type -> Type -> Type) where
+  dimap :: (a -> b) -> (c -> d) -> p b c -> p a d
+
+class Profunctor p => Strong p where
+  first' :: p a b -> p (a, c) (b, c)
+
+class Category (cat :: k -> k -> Type) where
+  id  :: forall (a :: k). cat a a
+  (.) :: forall (b :: k) (c :: k) (a :: k).
+         cat b c -> cat a b -> cat a c
+
+class (Category a, Profunctor a) => PreArrow a where
+  arr   :: (b -> c) -> a b c
+
+class (PreArrow a, Strong a) => Arrow a where
+  first :: a b c -> a (b, d) (c, d)
+  first = first'
+
+(>>>) :: forall k (a :: k) (b :: k) (c :: k) (cat :: k -> k -> Type).
+  Category cat => cat a b -> cat b c -> cat a c
+(>>>) = flip (.)
+{-- end Arrows --}
