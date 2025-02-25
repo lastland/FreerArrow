baa741646b500f4f2b6a90992d9cb110ebbd90e8
Yao Li <hnkfliyao@gmail.com>
2025-02-24 20:23:19 -0800

    Update.

--- src/Control/Arrow/Arrow.hs
+++ src/Control/Arrow/Arrow.hs
@@ -11,26 +11,29 @@ import Data.Kind
 class Profunctor (p :: Type -> Type -> Type) where
   dimap :: (a -> b) -> (c -> d) -> p b c -> p a d
 
-class Profunctor p => Strong p where
+class Profunctor p => StrongProfunctor p where
   first' :: p a b -> p (a, c) (b, c)
 
 class Category (cat :: k -> k -> Type) where
   id  :: forall (a :: k). cat a a
   (.) :: forall (b :: k) (c :: k) (a :: k).
          cat b c -> cat a b -> cat a c
-
+         
 class Category a => PreArrow a where
   arr   :: (b -> c) -> a b c
 
 class PreArrow a => Arrow a where
   first :: a b c -> a (b, d) (c, d)
 
+-- PreArrows are Profunctors
 instance PreArrow a => Profunctor a where
   dimap f g a = arr f >>> a >>> arr g
 
-instance (Strong a, PreArrow a) => Arrow a where
-  first = first'
+-- Arrows are StrongProfunctors
+instance Arrow a => StrongProfunctor a where
+  first' = first
 
+-- A commonly used operator for categories
 (>>>) :: forall k (a :: k) (b :: k) (c :: k) (cat :: k -> k -> Type).
   Category cat => cat a b -> cat b c -> cat a c
 (>>>) = flip (.)
