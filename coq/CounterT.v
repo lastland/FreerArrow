29890757e7511349172d8e09e37b538df2392400
Yao Li <hnkfliyao@gmail.com>
2025-03-03 16:06:39 -0800

    Adjustment.

--- coq/CounterT.v
+++ coq/CounterT.v
@@ -9,17 +9,14 @@ Instance Profunctor__CounterT {I} `{Profunctor I} : Profunctor (CounterT I) :=
   {| dimap := fun _ _ _ _ f g x => dimap f (fun '(y, n) => (g y, n)) x |}.
 
 Instance StrongProfunctor__CounterT {I} `{HS: StrongProfunctor I} : StrongProfunctor (CounterT I) :=
-  {| first' := fun _ _ C a => dimap (fun x => x) (fun '(x, n, y) => (x, y, n)) (@first' _ _ HS _ _ C a) |}.
+  {| first := fun _ _ C a => dimap (fun x => x) (fun '(x, n, y) => (x, y, n)) (@first _ _ HS _ _ C a) |}.
 
 Instance Category__CounterT {I} `{StrongProfunctor I} {HC : Category I} : Category (CounterT I) :=
   {| id := fun _ => dimap (fun x => x) (fun y => (y, 0)) id ;
-     comp := fun _ _ _ x y => dimap (fun x => x) (fun '(y, n1, n2) => (y, (n1 + n2)%nat)) (@comp I HC _ _ _ x (first' y)) |}.
+     comp := fun _ _ _ x y => dimap (fun x => x) (fun '(y, n1, n2) => (y, (n1 + n2)%nat)) (@comp I HC _ _ _ x (first y)) |}.
 
 Instance PreArrow__CounterT {I} `{StrongProfunctor I} {HC : Category I} `{PreArrow I} : PreArrow (CounterT I) :=
   {| arr := fun _ _ f => @comp I HC _ _ _ (arr f) (arr (fun y => (y, 0))) |}.
-
-Instance Arrow__CounterT {I} `{Arrow I} : Arrow (CounterT I) :=
-  {| first := fun _ _ _ => first' |}.
 
 Definition tick {I X Y} `{Arrow I} (i : I X Y) : CounterT I X Y :=
   dimap (fun x => x) (fun y => (y, 1)) i.
